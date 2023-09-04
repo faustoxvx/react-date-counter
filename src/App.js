af4194630ee497bb7a592b03237f01a96a758772
Fausto Nunes <61509756+faustoxvx@users.noreply.github.com>
2023-09-04 14:11:14 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <>
+      <div>
+        <button onClick={() => setStep((step) => step - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((step) => step + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((count) => count - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((count) => count + step)}>+</button>
+      </div>
+      <div>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count}  days from today will be `
+              : `${Math.abs(count)} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
+      </div>
+    </>
+  );
+}
